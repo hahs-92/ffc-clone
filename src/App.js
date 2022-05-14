4db544e335807b54b9d1924e3b3a3d5c643fccb2
hahs <alexandre_2420@outlook.com>
2022-05-14 12:13:05 -0500

    switch App to class component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import './App.css';
 //assets
 import shawnImg from './assets/images/testimonio-shawn.png'
@@ -7,37 +8,38 @@ import emmaImg from './assets/images/testimonio-emma.png'
 import CardTestimony  from './components/CardTestimony'
 
 
-function App() {
-  return (
-    <div className="App">
-      <main className='Container'>
-        <h1 className='Title'>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <CardTestimony
-          name='Shawn Wang'
-          country='Singapur'
-          image={ shawnImg }
-          role='Ingeniero de Software'
-          company=""
-          testimony='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
-        />
-        <CardTestimony
-          name='Sarah Chima'
-          country='Nigeria'
-          image={ sarahImg }
-          role='Ingeniera de Software'
-          company='ChatDesk'
-          testimony='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'/>
-        <CardTestimony
-          name='Emma Bostian'
-          country='Suecia'
-          image={ emmaImg }
-          role='Ingeniera de Software'
-          company='Spotify'
-          testimony='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'/>
-      </main>
-    </div>
-  );
-
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <main className='Container'>
+          <h1 className='Title'>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+          <CardTestimony
+            name='Shawn Wang'
+            country='Singapur'
+            image={ shawnImg }
+            role='Ingeniero de Software'
+            company=""
+            testimony='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
+          />
+          <CardTestimony
+            name='Sarah Chima'
+            country='Nigeria'
+            image={ sarahImg }
+            role='Ingeniera de Software'
+            company='ChatDesk'
+            testimony='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'/>
+          <CardTestimony
+            name='Emma Bostian'
+            country='Suecia'
+            image={ emmaImg }
+            role='Ingeniera de Software'
+            company='Spotify'
+            testimony='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'/>
+        </main>
+      </div>
+    );
+  }
 }
 
 
